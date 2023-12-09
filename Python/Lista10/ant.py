c8b64d91ca593771f77ab90a1c8dd51a1b94b2db
Julia Noczyńska <noczynska.julia@gmail.com>
2023-12-09 16:29:03 +0100

    Add Python 10-th list solutions

---
+++ Python/Lista10/ant.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+class LangtonAnt:
+    def __init__(self, width, height, max_steps, color, interval, direction):
+        self.width = width
+        self.height = height
+        self.interval = interval
+        self.color = color
+        self.direction = direction
+        self.max_steps = max_steps
+        self.steps = 0
+        self.ant_pos = (width // 2, height // 2)
+        self.dead_points = []
+
+
+    def turn_left(self):
+        dx, dy = self.direction
+        self.direction = (-dy, dx)
+    
+    def turn_right(self):
+        dx, dy = self.direction
+        self.direction = (dy, -dx)
+
+    def handle_dead_point(self):
+        self.dead_points.remove(self.ant_pos)
+        self.turn_left()
+    
+    def handle_living_point(self):
+        self.dead_points.append(self.ant_pos)
+        self.turn_right()
+    
+    def move(self):
+        new_x = self.ant_pos[0] + self.direction[0]
+        new_y = self.ant_pos[1] + self.direction[1]
+        self.ant_pos = (new_x,new_y)
+
+
+    def update(self, frame):
+        if self.steps == self.max_steps:
+            plt.title("Simulation Completed\nSteps: " + str(self.steps))
+            return
+
+        if self.ant_pos in self.dead_points:
+            self.handle_dead_point()
+        else:
+            self.handle_living_point()
+
+        self.move()
+        self.steps += 1
+        self.update_plot()
+
+
+    def update_plot(self):
+        plt.clf()
+        plt.scatter([x for x, y in self.dead_points], [y for x, y in self.dead_points], color=self.color, marker='s', s=5)
+        plt.scatter(self.ant_pos[0], self.ant_pos[1], color='red', marker='s', s=5)
+        plt.xlim(0, self.width)
+        plt.ylim(0, self.height)
+        plt.title("Steps: " + str(self.steps))
+    
+    
+    def start_simulation(self):
+        fig = plt.figure(num="Langton Ant", figsize=(8, 8))
+        anim = animation.FuncAnimation(fig, self.update, frames=1000, interval=self.interval)
+        def stop_animation(event):
+            print("Simulation Completed")
+
+        fig.canvas.mpl_connect('close_event', stop_animation)
+        plt.show()
+
+
+width = 100
+height = 100
+steps = 11500
+color = 'black'
+interval = 1
+direction = (1,0)
+
+simulation = LangtonAnt(width, height, steps, color, interval, direction)
+simulation.start_simulation()
---
+++ Python/Lista10/snake.py
@@ -0,0 +1,101 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import random
+
+
+class SnakeGame:
+    def __init__(self, width, height, num_blocks, num_points, snake_interval):
+        self.width = width
+        self.height = height
+        self.num_blocks = num_blocks
+        self.num_points = num_points
+        self.snake_interval = snake_interval
+
+        self.snake = [(width // 2, height // 2)]
+        self.direction = random.choice([(0, 1), (0, -1), (1, 0), (-1, 0)])
+        self.blocks = [self.generate_point() for _ in range(num_blocks)]
+        self.points = [self.generate_point() for _ in range(num_points)]
+        self.game_over = False
+
+
+    def generate_point(self):
+        return (random.randint(1, width-2),random.randint(1, height-2))
+    
+
+    def update(self, frame):
+        if self.game_over:
+            plt.title("Game Over")
+            return
+
+        head_x, head_y = self.snake[-1]
+        new_head = (head_x + self.direction[0], head_y + self.direction[1])
+    
+        if self.check_for_collision(new_head):
+            self.game_over = True
+            return
+        
+        self.move(new_head)
+        self.update_plot()
+
+
+    def check_for_collision(self, new_head):
+        with_snake = new_head in self.snake[:-1]
+        with_map_border = not (0 <= new_head[0] <= self.width) or not (0 <= new_head[1] <= self.height)
+        with_blocks = new_head in self.blocks
+        return with_snake or with_map_border or with_blocks
+    
+
+    def move (self, new_head):
+        self.snake.append(new_head)
+
+        if new_head in self.points:
+            self.points.remove(new_head)
+            self.points.append(self.generate_point())
+        else:
+            self.snake.pop(0)
+
+
+    def update_plot(self):
+        plt.clf()
+        plt.scatter([x for x, y in self.blocks], [y for x, y in self.blocks], color='red', marker='s', s=100)
+        plt.scatter([x for x, y in self.snake], [y for x, y in self.snake], color='blue', marker='s', s=100)
+        plt.scatter([x for x, y in self.points], [y for x, y in self.points], color='green', marker='s', s=100)
+        plt.xlim(0, self.width)
+        plt.ylim(0, self.height)
+        plt.title("Points: " + str(len(self.snake) - 1))
+    
+
+    def change_direction_on_key(self, event):
+        new_direction = self.direction
+        if event.key == 'up':
+            new_direction = (0, 1)
+        elif event.key == 'down':
+            new_direction = (0, -1)
+        elif event.key == 'right':
+            new_direction = (1, 0)
+        elif event.key == 'left':
+            new_direction = (-1, 0)
+        if (self.direction[0] + new_direction[0] != 0) or (self.direction[1] + new_direction[1] != 0):    
+            self.direction = new_direction
+    
+    
+    def start_game(self):
+        fig = plt.figure(num="Snake", figsize=(8, 8))
+        fig.canvas.mpl_connect('key_press_event', self.change_direction_on_key)
+        anim = animation.FuncAnimation(fig, self.update, frames=1000, interval=self.snake_interval)
+        def stop_animation(event):
+            self.game_over = True
+            print("Game Over")
+
+        fig.canvas.mpl_connect('close_event', stop_animation)
+        plt.show()
+
+
+width = 25
+height = 25
+num_blocks = 2
+num_points = 1
+snake_interval = 50
+
+game = SnakeGame(width, height, num_blocks, num_points, snake_interval)
+game.start_game()
