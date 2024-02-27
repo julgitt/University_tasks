7b610da6cbbd5caef769b88f8b2c632682639917
Julia Noczyńska <noczynska.julia@gmail.com>
2024-02-28 00:13:12 +0100

    Simplify the solution of AI 5-th task from 1-st lab

--- sztuczna_inteligencja/pracownia1/zad5/image.py
+++ sztuczna_inteligencja/pracownia1/zad5/image.py
@@ -1,6 +1,6 @@
-import copy
-import random
-from typing import List
+from copy import copy, deepcopy
+from random import choice
+from typing import List, Tuple
 
 class Image:
     
@@ -12,118 +12,104 @@ class Image:
         self.image = []
         self.rotated_image = []
     
-    #region getters    
-    def get_row(self, index: int) -> List[str]:
+    #region Getters
+    def _get_row(self, index: int) -> List[str]:
         return self.image[index]
 
-    def get_column(self, index: int) -> List[str]:
+    def _get_column(self, index: int) -> List[str]:
         return self.rotated_image[index]
     #endregion
     
-    #region setters
-    def set_row(self, index: int, new_row: str):
-        self.image[index] = new_row
-        self.update_rotated_image()
-    
-    def set_column(self, index, new_column):
-        self.rotated_image[index] = new_column  
-        self.update_image()
-    #endregion
-    
-    #region updating_image    
-    def swap_cell(self, row_id, col_id):
+    #region Image Update   
+    def swap_cell(self, row_id: int, col_id: int) -> None:
         row = self.image[row_id]
         current_char = row[col_id]
         new_char = '#' if current_char == '.' else '.'
         self.image[row_id] = row[:col_id] + new_char + row[col_id + 1:]
-        self.update_rotated_image()
+        self._update_rotated_image()
         
-    def regenerate(self):
+    def regenerate(self) -> None:
         self.image = []
         for _ in range(self.height):
-            self.image.append(''.join(random.choice('.#') for _ in range(self.width)))
-        self.update_rotated_image()
+            self.image.append(''.join(choice('.#') for _ in range(self.width)))
+        self._update_rotated_image()
             
-    def update_rotated_image(self):
+    def _update_rotated_image(self) -> None:
         self.rotated_image = []
         for i in range(self.width):
             self.rotated_image.append("".join([line[i] for line in self.image]))
     
-    def update_image(self):
-        self.image = []
-        for i in range(self.width):
-            self.image.append("".join([line[i] for line in self.rotated_image]))
     #endregion
     
-    #region incorrect_lines_getters     
-    def get_random_incorrect_row_id(self):
-        incorrect_rows = [i for i in range(self.height) if self.opt_dist(self.get_row(i), self.rows_blocks[i]) > 0]
-        return random.choice(incorrect_rows) if incorrect_rows else -1
+    #region Incorrect Lines Getters     
+    def get_random_incorrect_row_id(self) -> int:
+        incorrect_rows = [i for i in range(self.height) if self._opt_dist(self._get_row(i), self.rows_blocks[i]) > 0]
+        return choice(incorrect_rows) if incorrect_rows else -1
 
-    def get_random_incorrect_column_id(self):
-        incorrect_columns = [i for i in range(self.width) if self.opt_dist(self.get_column(i), self.columns_blocks[i]) > 0]
-        return random.choice(incorrect_columns) if incorrect_columns else -1
+    def get_random_incorrect_column_id(self) -> int:
+        incorrect_columns = [i for i in range(self.width) if self._opt_dist(self._get_column(i), self.columns_blocks[i]) > 0]
+        return choice(incorrect_columns) if incorrect_columns else -1
     #endregion
     
-    #region algorithm_functions
-    def opt_dist(self, line, block_size):
+    #region Find Best Pick
+    def find_best_cell_in_row(self, row_id: int) -> Tuple[int, int]:
+        best_opers = 100   # how many operations to correct 
+        best_cell = 0      # column index of the best_cell
+        
+        for col_id in range(0, self.width):
+            img_copy = deepcopy(self)
+            img_copy.swap_cell(row_id, col_id)
+            current_opers = img_copy._opt_dist(img_copy._get_row(row_id), img_copy.rows_blocks[row_id])
+            for i in range(img_copy.width):
+                current_opers += img_copy._opt_dist(img_copy._get_column(i), img_copy.columns_blocks[i])
+            if current_opers < best_opers:
+                best_opers = current_opers
+                best_cell = col_id
+                
+        return best_cell
+    
+    def find_best_cell_in_column(self, col_id: int) -> Tuple[int, int]:
+        best_opers = 100  # how many operations to correct 
+        best_cell = 0     # column index of the best_cell
+        
+        for row_id in range(0, self.height):
+            img_copy = deepcopy(self)
+            img_copy.swap_cell(row_id, col_id)
+            current_opers = img_copy._opt_dist(img_copy._get_column(col_id), img_copy.columns_blocks[col_id])
+            for i in range(img_copy.height):
+                current_opers += img_copy._opt_dist(img_copy._get_row(i), img_copy.rows_blocks[i])
+            if current_opers < best_opers:
+                best_opers = current_opers
+                best_cell = row_id
+                
+        return best_cell
+    
+    def _opt_dist(self, line: str, block_size: int) -> int:
         min_operations = float('inf')
         
         for i in range(len(line) - block_size + 1):
-            operations = line.count('#', 0, i) + line.count('.', i, i + block_size) + line.count('#', block_size + i, len(line))
+            operations = line.count('#', 0, i) \
+                        + line.count('.', i, i + block_size) \
+                        + line.count('#', block_size + i, len(line))
             if min_operations > operations: 
                 min_operations = operations
                 
         return min_operations
-    
-
-    def find_best_cell_in_row(self, i):
-        best_opers = 100   # how many operations to correct 
-        best_cell = 0    # column index of the best_cell
-        
-        for k in range(0, self.width):
-            img_copy = copy.deepcopy(self)
-            img_copy.swap_cell(i, k)
-            current_opers = img_copy.opt_dist(img_copy.get_row(i), img_copy.rows_blocks[i])
-            for j in range(img_copy.width):
-                current_opers += img_copy.opt_dist(img_copy.get_column(j), img_copy.columns_blocks[j])
-            if current_opers < best_opers:
-                best_opers = current_opers
-                best_cell = k
-                
-        return best_cell, best_opers
-    
-    def find_best_cell_in_column(self, i):
-        best_opers = 100  # how many operations to correct 
-        best_cell = 0    # column index of the best_cell
-        
-        for k in range(0, self.height):
-            img_copy = copy.deepcopy(self)
-            img_copy.swap_cell(k, i)
-            current_opers = img_copy.opt_dist(img_copy.get_column(i), img_copy.columns_blocks[i])
-            for j in range(img_copy.height):
-                current_opers += img_copy.opt_dist(img_copy.get_row(j), img_copy.rows_blocks[j])
-            if current_opers < best_opers:
-                best_opers = current_opers
-                best_cell = k
-                
-        return best_cell, best_opers
     #endregion
     
-    #region object_copy
+    #region Image Copy
     def __copy__(self):
         new_image = self.__class__(self.height, self.width, self.columns_blocks, self.rows_blocks)
-        new_image.image = copy.copy(self.image)
-        new_image.rotated_image = copy.copy(self.rotated_image)
+        new_image.image = copy(self.image)
+        new_image.rotated_image = copy(self.rotated_image)
         return new_image
 
-    # Implementacja metody __deepcopy__ dla glebokiej kopii
     def __deepcopy__(self, memo=None):
         if memo is None:
             memo = {}
         new_image = self.__class__(self.height, self.width, self.columns_blocks, self.rows_blocks)
         memo[id(self)] = new_image
-        new_image.image = copy.deepcopy(self.image, memo)
-        new_image.rotated_image = copy.deepcopy(self.rotated_image, memo)
+        new_image.image = deepcopy(self.image, memo)
+        new_image.rotated_image = deepcopy(self.rotated_image, memo)
         return new_image
     #endregion
--- sztuczna_inteligencja/pracownia1/zad5/imageSolver.py
+++ sztuczna_inteligencja/pracownia1/zad5/imageSolver.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List
 
 from image import Image
 
@@ -10,6 +10,7 @@ class ImageSolver:
         self.columns_blocks = columns_blocks
         self.rows_blocks = rows_blocks
         self.image = Image(height, width, columns_blocks, rows_blocks)
+        self.prev_operations = 0
         
     
     def solve(self, max_iterations: int) -> List[str]:
@@ -26,30 +27,9 @@ class ImageSolver:
                     return self.image.image   
             
     def _handle_row(self, id: int) -> None:
-        column_id, operations = self.image.find_best_cell_in_row(id)
-        if operations >= 17:
-            row_to_change = self.image.get_row(id)
-            self.image.set_row(id, self._change_line(row_to_change, self.rows_blocks[id]))
-        else:     
-            self.image.swap_cell(id, column_id)
+        column_id = self.image.find_best_cell_in_row(id) 
+        self.image.swap_cell(id, column_id)
         
     def _handle_column(self, id: int) -> None:
-        row_id, operations = self.image.find_best_cell_in_column(id)
-        if operations >= 17:
-            column_to_change = self.image.get_column(id)
-            self.image.set_column(id, self._change_line(column_to_change, self.columns_blocks[id]))
-        else: 
-            self.image.swap_cell(row_id, id)
-        
-    @staticmethod
-    def _change_line(line: str, block_size: str) -> str:
-        min_operations = float('inf')
-        fixed_line = line
-
-        for i in range(len(line) - block_size + 1):
-            operations = line.count('#', 0, i) + line.count('.', i, i + block_size) + line.count('#', block_size + i, len(line))
-            if min_operations > operations: 
-                min_operations = operations
-                fixed_line = '.' * i + '#' * block_size + '.' * (len(line) - block_size - i)
-        
-        return fixed_line
+        row_id = self.image.find_best_cell_in_column(id)
+        self.image.swap_cell(row_id, id)
