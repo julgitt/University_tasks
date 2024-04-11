59d07b45fd65704180d6cc9851a09e19a73e1566
Julia Noczyńska <noczynska.julia@gmail.com>
2024-04-11 19:39:31 +0200

    Add AI task solution

--- sztuczna_inteligencja/pracownia3/zad4/zad4.py
+++ sztuczna_inteligencja/pracownia3/zad4/zad4.py
@@ -1,4 +1,3 @@
-
 def B(i, j):
     return 'B_%d_%d' % (i, j)
 
@@ -7,58 +6,43 @@ def domains(Vs):
     return [q + ' in 0..1' for q in Vs]
 
 
+def get_row(i, cols_num):
+    return [B(i, j) for j in range(cols_num)]
+
+
+def get_column(j, rows_num):
+    return [B(i, j) for i in range(rows_num)]
+
+
+def is_sum_equal(Qs, q):
+    return 'sum([' + ', '.join(Qs) + '], #=, ' + str(q) + ')'
+
+
 def check_rows(rows, cols_num):
-    result = []
-    for i, row in enumerate(rows):
-        all_vals_in_row = [B(i, j) for j in range(cols_num)]
-        result.append('sum([' + ', '.join(all_vals_in_row) + '], #=, ' + str(row) + ')')
-    return result
+    return [is_sum_equal(get_row(i, cols_num), row) for i, row in enumerate(rows)]
 
 
 def check_cols(cols, rows_num):
-    result = []
-    for j, col in enumerate(cols):
-        all_vals_in_col = [B(i, j) for i in range(rows_num)]
-        result.append('sum([' + ', '.join(all_vals_in_col) + '], #=, ' + str(col) + ')')
-    return result
+    return [is_sum_equal(get_column(i, rows_num), col) for i, col in enumerate(cols)]
 
 
 # A + 2B + 3C != 2
 # Jeżeli środkowy piksel jest ustawiony, to wówczas przynajmniej 1 z
 # otaczających go jest jedynką.
 def good_three_cells_in_rows(cols_num, rows_num):
-    result = []
-    for j in range(cols_num):
-        for i in range(rows_num - 2):
-            a = B(i, j)
-            b = B(i + 1, j)
-            c = B(i + 2, j)
-            result.append(b + ' #= 1 #==> ' + a + ' + ' + c + ' #>= 1')
-    return result
+    return [f"{B(i + 1, j)} #= 1 #==> {B(i, j)} + {B(i + 2, j)} #>= 1"
+            for i in range(rows_num - 2) for j in range(cols_num)]
 
 
 def good_three_cells_in_cols(cols_num, rows_num):
-    result = []
-    for j in range(cols_num - 2):
-        for i in range(rows_num):
-            a = B(i, j)
-            b = B(i, j + 1)
-            c = B(i, j + 2)
-            result.append(b + ' #= 1 #==> ' + a + ' + ' + c + ' #>= 1')
-    return result
+    return [f"{B(i, j + 1)} #= 1 #==> {B(i, j)} + {B(i, j + 2)} #>= 1"
+            for i in range(rows_num) for j in range(cols_num - 2)]
 
 
 # B × (A + C) != 2
 def rectangle(cols_num, rows_num):
-    result = []
-    for j in range(cols_num - 1):
-        for i in range(rows_num - 1):
-            a = B(i, j)
-            b = B(i, j + 1)
-            c = B(i + 1, j)
-            d = B(i + 1, j + 1)
-            result.append(a + ' + ' + d + ' #= 2 #<==> ' + b + ' + ' + c + ' #= 2')
-    return result
+    return [f"{B(i, j)} + {B(i + 1, j + 1)} #= 2 #<==> {B(i, j + 1)} + {B(i + 1, j)} #= 2"
+            for j in range(cols_num - 1) for i in range(rows_num - 1)]
 
 
 def print_constraints(Cs, indent, d):
@@ -74,17 +58,18 @@ def print_constraints(Cs, indent, d):
 
 def storms(rows, cols, triples):
     writeln(':- use_module(library(clpfd)).')
-    
+
     R = len(rows)
     C = len(cols)
-    
-    bs = [B(i, j) for i in range(R) for j in range(C)]
-    
-    writeln('solve([' + ', '.join(bs) + ']) :- ')
-    
-    #TODO: add some constraints
 
-    cs = domains(bs) + check_rows(rows, C) + check_cols(cols, R) + good_three_cells_in_cols(C, R) + good_three_cells_in_rows(C, R) + rectangle(C, R)
+    bs = [B(i, j) for i in range(R) for j in range(C)]
+
+    writeln('solve([' + ', '.join(bs) + ']) :- ')
+
+    # TODO: add some constraints
+
+    cs = domains(bs) + check_rows(rows, C) + check_cols(cols, R) + good_three_cells_in_cols(C, R) \
+         + good_three_cells_in_rows(C, R) + rectangle(C, R)
 
     for i, j, val in triples:
         cs.append('%s #= %d' % (B(i, j), val))
@@ -111,6 +96,4 @@ for i in range(2, len(txt)):
     if txt[i].strip():
         triples.append(map(int, txt[i].split()))
 
-storms(rows, cols, triples)            
-        
-
+storms(rows, cols, triples)
