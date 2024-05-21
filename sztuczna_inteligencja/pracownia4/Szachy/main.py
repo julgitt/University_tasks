b71d4cbccd2f34a47d902fd91535b2b3855cc72e
Julia Noczyńska <noczynska.julia@gmail.com>
2024-05-22 01:15:17 +0200

    Add working chess bot from AI

--- sztuczna_inteligencja/pracownia4/Szachy/main.py
+++ sztuczna_inteligencja/pracownia4/Szachy/main.py
@@ -1,6 +1,7 @@
-import sys
-from monteCarloTreeSearch import Node, MCTS
+from sys import stdout
 import chess
+
+from monteCarloTreeSearch import Node, reset_board, MCTS
 
 
 def read_input():
@@ -8,37 +9,71 @@ def read_input():
 
 
 def send_output(output):
-    sys.stdout.flush()
+    stdout.flush()
     print(output)
-    sys.stdout.flush()
+    stdout.flush()
 
 
 class Program:
-    if __name__ == "__main__":
-        send_output("RDY")
-        board = chess.Board()
+    if __name__ == '__main__':
+        tree = Node(False, None, None, chess.BLACK)
         mcts = MCTS()
-        mcts_tree = Node(board, False, None, None, 1)
-
+        send_output("RDY")
         while True:
-            command = read_input()
+            command = input().split()
             if command[0] == "UGO":
-                move_time = float(command[1])
-                game_time = float(command[2])
-                mcts_tree, m = mcts.next(Node(board, False, None, None, 0))
-                send_output(f"IDO {m}")
+                tree = Node(False, None, None, chess.WHITE)
+                tree = mcts.search(tree)
+                tree, move = tree.next()
+                send_output(f"IDO {move}")
             elif command[0] == "HEDID":
-                move_time = float(command[1])
-                game_time = float(command[2])
-                # opponents move
-                opponent_move = command[3]
-                mcts_tree = mcts.next_opponent(mcts_tree, opponent_move)
-                # our move
-                mcts_tree, m = mcts.next(mcts_tree)
-                send_output(f"IDO {m}")
+                move = command[3]
+                tree = tree.next_opponent(move)
+                tree = mcts.search(tree)
+                tree, move = tree.next()
+                send_output(f"IDO {move}")
             elif command[0] == "ONEMORE":
+                tree = Node(False, None, None, chess.BLACK)
+                reset_board()
                 send_output("RDY")
-                board = chess.Board()
-                mcts_tree = Node(board, False, None, None, 1)
             elif command[0] == "BYE":
                 break
+
+# def read_input():
+#     return input().split()
+#
+#
+# def send_output(output):
+#     sys.stdout.flush()
+#     print(output)
+#     sys.stdout.flush()
+#
+#
+# class Program:
+#     if __name__ == '__main__':
+#         send_output("RDY")
+#         mcts = MCTS()
+#         mcts_tree = Node(False, None, None, 1)
+#
+#         while True:
+#             command = read_input()
+#             if command[0] == "UGO":
+#                 move_time = float(command[1])
+#                 game_time = float(command[2])
+#                 mcts_tree, m = mcts.next(Node(False, None, None, 0))
+#                 send_output(f"IDO {m}")
+#             elif command[0] == "HEDID":
+#                 move_time = float(command[1])
+#                 game_time = float(command[2])
+#                 # opponents move
+#                 opponent_move = command[3]
+#                 mcts_tree = mcts.next_opponent(mcts_tree, opponent_move)
+#                 # our move
+#                 mcts_tree, m = mcts.next(mcts_tree)
+#                 send_output(f"IDO {m}")
+#             elif command[0] == "ONEMORE":
+#                 send_output("RDY")
+#                 reset_board()
+#                 mcts_tree = Node(False, None, None, 1)
+#             elif command[0] == "BYE":
+#                 break
--- sztuczna_inteligencja/pracownia4/Szachy/monteCarloTreeSearch.py
+++ sztuczna_inteligencja/pracownia4/Szachy/monteCarloTreeSearch.py
@@ -1,66 +1,80 @@
-from random import choice
-from typing import Optional, Tuple
-from math import sqrt, log
-from copy import deepcopy
-import chess
+from sys import stderr
 from time import time
-C = 1.5
+from typing import Optional, Tuple, Dict
+from math import sqrt, log
+from random import choice
+
+import chess
+
+C = 1.0
+
+board = chess.Board()
+
+
+def reset_board():
+    global board
+    board = chess.Board()
 
 
 class Node:
-    def __init__(self, state: chess.Board, done: bool, parent: Optional['Node'],
-                 action: Optional[chess.Move], agent: chess.COLORS):
-        self.children: dict = {}
-        self.total_reward: int = 0
-        self.visit_count: int = 0
-        self.game: chess.Board = state
-        self.done: bool = done
-        self.parent: Optional[Node] = parent
-        self.action: chess.Move = action
-        self.agent: int = agent
 
-    def get_ucb_score(self) -> float:
-        if self.visit_count == 0:
+    def __init__(self, game_over: bool, parent: Optional['Node'], move: Optional[chess.Move], agent_player: int):
+        self.children: Dict[chess.Move, Node] = {}
+        self.heuristics_sum: int = 0
+        self.visit_count: int = 0
+        self.game_over: bool = game_over
+        self.parent: Optional['Node'] = parent
+        self.move: chess.Move = move
+        self.agent_player: int = agent_player
+
+    def ucb_score(self) -> float:
+        if self.heuristics_sum == 0:
             return float("inf")
 
-        top_node = self
-        if top_node.parent:
-            top_node = top_node.parent
-
-        return self.total_reward / self.visit_count + 1 * sqrt(log(top_node.visit_count) / self.visit_count)
+        return self.heuristics_sum / self.visit_count + C * sqrt(log(self.visit_count
+                                                                     if not self.parent
+                                                                     else self.parent.visit_count)
+                                                                 / self.visit_count)
 
     def expand(self) -> None:
-        if self.done:
+        if self.game_over:
             return
 
         children = {}
-        moves = self.game.legal_moves
-        for move in moves:
-            new_game = deepcopy(self.game)
-            new_game.push(move)
-            children[move] = Node(new_game, new_game.is_game_over(), self, move, self.agent)
+        legal_moves = board.legal_moves
+        for m in legal_moves:
+            board.push(m)
+            children[m] = Node(board.is_game_over(), self, m, self.agent_player)
+            board.pop()
 
         self.children = children
 
     def explore(self) -> None:
         current = self
 
+        iterations = 0
         while current.children:
             children = current.children
-            max_score = max(c.get_ucb_score() for c in children.values())
-            actions = [a for a, c in children.items() if c.get_ucb_score() == max_score]
-            if not actions:
-                print("error zero length ", max_score)
-            action = choice(actions)
-            current = children[action]
+            if not children:
+                print("There are no children in explore", stderr)
+            max_node_score = max(c.ucb_score() for c in children.values())
+            moves = [a for a, c in children.items() if c.ucb_score() == max_node_score]
+            move = choice(moves)
+            board.push(move)
+            current = children[move]
+            iterations += 1
 
         if current.visit_count < 1:
-            current.total_reward += current.simulation()
+            current.heuristics_sum += current.run_simulation()
         else:
             current.expand()
             if current.children:
                 current = choice(list(current.children.values()))
-            current.total_reward += current.simulation()
+                board.push(current.move)
+                current.heuristics_sum += current.run_simulation()
+                board.pop()
+            else:
+                current.heuristics_sum += current.run_simulation()
 
         current.visit_count += 1
 
@@ -69,126 +83,123 @@ class Node:
         while parent.parent:
             parent = parent.parent
             parent.visit_count += 1
-            parent.total_reward += current.total_reward
+            parent.heuristics_sum += current.heuristics_sum
 
-    def simulation(self):
-        if self.done:
+        for i in range(iterations):
+            board.pop()
+
+    def run_simulation(self) -> int:
+        if self.game_over:
             return 0
-        v = 0
-        new_game = deepcopy(self.game)
-        for _ in range(20):
-            if new_game.is_game_over():
-                break
-            action = choice(list(new_game.legal_moves))
-            new_game.push(action)
-            v += heuristic(new_game, self.agent)
 
+        v = 0
+        i = 0
+        for i in range(20):
+            if board.is_game_over():
+                break
+            move = choice(list(board.legal_moves))
+            board.push(move)
+            v += Node.heuristic(board, self.agent_player)
+            i += 1
+        for j in range(i):
+            board.pop()
         return v
 
     def next(self) -> Tuple['Node', str]:
-        if self.done:
-            raise ValueError("game has ended")
+        if self.game_over:
+            print("Game over in next", stderr)
 
         if not self.children:
-            raise ValueError('no children found and game hasn\'t ended')
+            print("There are no children in next", stderr)
 
         children = self.children
 
         max_visit_count = max(node.visit_count for node in children.values())
-
         max_children = [c for a, c in children.items() if c.visit_count == max_visit_count]
 
-        #if len(max_children) == 0:
-        #    print("error zero length ", max_visit_count)
-
         max_child = choice(max_children)
-
         max_child.parent = None
+        board.push(max_child.move)
 
-        return max_child, max_child.action.uci()
+        return max_child, max_child.move.uci()
 
     def next_opponent(self, move: str) -> 'Node':
-        if self.done:
-            raise ValueError("game has ended")
+        if self.game_over:
+            print("Game over in next_opponent", stderr)
 
         if not self.children:
             self.expand()
 
         children = self.children
 
-        for m, c in list(children.items()):
+        for m, child in list(children.items()):
             if m.uci() == move:
-                c.parent = None
-                return c
+                child.parent = None
+                board.push(m)
+                return child
 
-        raise ValueError("Move is not possible")
+        print("Opponent has not found the move", stderr)
 
+    @staticmethod
+    def checkmate_heuristic(game: chess.Board, player: int) -> float:
+        if game.is_checkmate():
+            if game.turn == player:
+                return -float("inf")
+            return float("inf")
+        return 0
 
-def check(game: chess.Board, player: chess.COLORS) -> float:
-    if game.is_checkmate():
-        if game.turn == player:
-            return -float("inf")
-        return float("inf")
-    return 0
+    @staticmethod
+    def mobility_heuristic(game: chess.Board, player: int) -> int:
+        res = 0
+        if game.legal_moves:
+            for move in game.legal_moves:
+                if game.color_at(move.from_square) == player:
+                    res += 1
+                else:
+                    res -= 1
+        return res
 
+    @staticmethod
+    def score_heuristic(game: chess.Board, player: int) -> int:
+        piece_values = {
+            chess.PAWN: 1,
+            chess.KNIGHT: 3,
+            chess.BISHOP: 3,
+            chess.ROOK: 5,
+            chess.QUEEN: 9
+        }
+        res = 0
 
-def mobility(game: chess.Board, player: chess.COLORS) -> int:
-    res = 0
-    if game.legal_moves:
-        for move in game.legal_moves:
-            if game.color_at(move.from_square) == player:
-                res += 1
-            else:
-                res -= 1
-    return res
+        for square in chess.SQUARES:
+            piece = game.piece_at(square)
+            if piece:
+                value = piece_values.get(piece.piece_type, 0)
+                if piece.color == player:
+                    res += value
+                else:
+                    res -= value
+        return res
 
-
-def score_heuristic(game: chess.Board, player: chess.COLORS) -> int:
-    piece_values = {
-        chess.PAWN: 1,
-        chess.KNIGHT: 3,
-        chess.BISHOP: 3,
-        chess.ROOK: 5,
-        chess.QUEEN: 9
-    }
-    white = 0
-    black = 0
-
-    for square in chess.SQUARES:
-        piece = game.piece_at(square)
-        if piece:
-            value = piece_values.get(piece.piece_type, 0)
-            if piece.color == chess.WHITE:
-                white += value
-            else:
-                black += value
-    if player == 0:
-        return white - black
-
-    return black - white
-
-
-def heuristic(game: chess.Board, player: int) -> float:
-    return score_heuristic(game, player) + 0.5 * mobility(game, player) + check(game, player)
+    @staticmethod
+    def heuristic(game: chess.Board, player: int) -> float:
+        return Node.score_heuristic(game, player) + 0.5 * Node.mobility_heuristic(game,
+                                                                                  player) + Node.checkmate_heuristic(
+            game, player)
 
 
 class MCTS:
-    def __init__(self, explores: int = 100):
-        self.explores = explores
+    def __init__(self):
+        self.explores = 2
 
-    @staticmethod
-    def next(tree: Node) -> Tuple[Node, str]:
-        start = time()
-        while time() - start < 0.60:
-        #for i in range(100):
+    def search(self, tree):
+        start_time = time()
+        explores = self.explores
+        for i in range(explores):
             tree.explore()
 
-        next_tree, next_action = tree.next()
+        if time() - start_time > 0.5 and self.explores > 2:
+            self.explores -= 1
+        else:
+            self.explores += 1
 
-        return next_tree, next_action
-
-    @staticmethod
-    def next_opponent(tree: Node, move: str) -> Node:
-        next_tree = tree.next_opponent(move)
-
-        return next_tree
+        return tree
